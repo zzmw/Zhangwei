0f15f0acc68308cf3e9dab0280aacee9df4a1279
zzmw <48405930+zzmw@users.noreply.github.com>
2019-03-17 21:44:01 +0800

    Add files via upload

---
+++ homework 2.7.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public class ArrayProgram1
+{
+    public static void Main()
+    {
+        Console.WriteLine("给长度为10的数组赋初值：\n");
+        int[] par = new int[10];
+        for (int i = 0; i < 10; i++)
+        {
+            string s = Console.ReadLine();
+            par[i] = int.Parse(s);
+        }
+        int min = par[0], max = par[0], sum = 0, ave = 0;
+        for (int i = 0; i < par.Length; i++)
+        {
+            if (min > par[i])
+                min = par[i];
+            if (max < par[i])
+                max = par[i];
+            sum = sum + par[i];
+        }
+        ave = sum / par.Length;
+        Console.WriteLine("最小数：" + min);
+        Console.WriteLine("最大数：" + max);
+        Console.WriteLine("平均数：" + ave);
+        Console.WriteLine("和：" + sum);
+    }
+}
---
+++ homework 2.9.cs
@@ -0,0 +1,33 @@
+﻿using System;
+public class SelectPrime
+{
+    public static void Main()
+    {
+        int a;
+        int[] p = new int[99];
+        for (int i=0;i<=98;i++)
+        {
+            p[i] = i + 2;
+        }
+        for(int i=0;i<=98;i++)
+        {
+            if (p[i] != 0)
+            {
+                a = p[i];
+                for (int j = 0; j <= 98; j++)
+                {
+                    if (p[j] % a == 0)
+                        p[j] = 0;
+                }
+            }
+            else
+                break;
+        }
+        for(int t=0;t<=98;t++)
+        {
+            if (p[t] != 0)
+                Console.WriteLine(p[t] + " ");
+        }
+    }
+
+}
