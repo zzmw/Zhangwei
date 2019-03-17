e28ac01c667e074f54c105f574d51aab63fa7806
zzmw <48405930+zzmw@users.noreply.github.com>
2019-03-17 21:39:14 +0800

    Add files via upload
    作业2.6

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class GetPrimeNumber
+{
+    public static void Main()
+    {
+        int i = 2;
+        int n;
+        string s;
+        Console.WriteLine("请输入一个整数：\n");
+        s= Console.ReadLine();
+        n = int.Parse(s);
+        Console.WriteLine("该整数的全部素数因子是：\n");
+        while (i <= n)
+        {
+            if (n % i == 0)
+            {
+                Console.Write(value:i);
+                n /= i;
+            }
+            else
+                i++;
+        }
+    }
+}
+
